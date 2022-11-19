019efd0b842d629d4efd620859c816401af40a9b
TytanowyBartosz <bartoszjanjerzy@gmail.com>
2022-11-19 15:21:48 +0100

    base model saved

---
+++ image/augmentation.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+
+
+class AugmentationPipeline:
+  
+  def load_augmented_images(self, base_images: list) -> list:
+      shifted_images = self._vertical_shift(base_images)
+      bright_images = self._brightness(base_images)
+      rotated_images = self._rotation(base_images)
+      sobel_images = self._edge_detection(base_images)
+      
+      return shifted_images + bright_images + rotated_images + sobel_images
+  
+  def _vertical_shift(self, images):
+    final_images = []
+    for img in images:
+      ratio = np.random.randint(0, 101) / 100
+      ratio = np.random.uniform(-ratio, ratio)
+      h, w = img.shape[:2]
+      to_shift = h*ratio
+      
+      if ratio > 0:
+          aug_img = img[:int(h-to_shift), :, :]
+      
+      if ratio < 0:
+          aug_img = img[int(-1*to_shift):, :, :]
+
+      final_images.append(aug_img)
+    
+    return final_images
+
+  def _brightness(self, images: list) -> list:
+    final_images = []
+    for img in images:
+      low = np.random.randint(0, 10)
+      high = np.random.randint(11, 30)
+      value = np.random.uniform(low, high)
+      
+      hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+      hsv = np.array(hsv, dtype = np.float64)
+      hsv[:,:,1] = hsv[:,:,1]*value
+      hsv[:,:,1][hsv[:,:,1]>255]  = 255
+      hsv[:,:,2] = hsv[:,:,2]*value 
+      hsv[:,:,2][hsv[:,:,2]>255]  = 255
+      hsv = np.array(hsv, dtype = np.uint8)
+      aug_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+      final_images.append(aug_img)
+      
+    return final_images
+
+  def _rotation(self, images: list) -> list:
+      final_images = []
+      for img in images:
+        low = np.random.randint(0, 10)
+        high = np.random.randint(11, 30)
+        angle = np.random.uniform(low, high)
+        
+        h, w = img.shape[:2]
+        M = cv2.getRotationMatrix2D((int(w/2), int(h/2)), angle, 1)
+        aug_img = cv2.warpAffine(img, M, (w, h))
+        
+        final_images.append(aug_img)
+      
+      return final_images
+  
+  def _edge_detection(self, images: list) -> list:
+    final_images = []
+    
+    for img in images:
+      img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      img_blur = cv2.GaussianBlur(img_gray, (3,3), 0) 
+      aug_img = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+      final_images.append(aug_img)
+    
+    return final_images
+      
---
+++ process/utils.py
@@ -0,0 +1,49 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from typing import List
+
+
+def load_history_graph(
+    acc: List[float],
+    val_acc: List[float],
+    loss: List[float],
+    val_loss: List[float],
+    title='History',
+    width: int=1300,
+    height: int=600
+) -> go.Figure:
+    x = [i+1 for i in range(len(acc))]
+    
+    fig = make_subplots(
+        cols=2, rows=1,
+        subplot_titles=('Accuracy', 'Loss')
+    )
+    
+    for y, name, col, color in zip(
+        [acc, val_acc, loss, val_loss],
+        ['Accuracy', 'Val Accuracy', 'Loss', 'Val Loss'],
+        [1, 1, 2, 2],
+        ['blue', 'orange', 'blue', 'orange']
+    ):
+        fig.add_trace(
+            trace=go.Scatter(
+                name=name,
+                x=x,
+                y=y,
+                mode='lines+markers',
+                marker_color=color
+            ),
+            col=col, row=1
+        )
+    
+    fig.update_yaxes(range=(0, max(acc)+0.1), col=2)
+    fig.update_yaxes(range=(0, max(val_loss)+0.2), col=1)
+    
+    fig.update_layout(
+        title=title,
+        width=width,
+        height=height,
+        xaxis_title='epoch'
+    )
+    
+    return fig
